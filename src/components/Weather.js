dd903b45d86d065fd97575585eda64babe01c2eb
poopysmack <54606838+poopysmack@users.noreply.github.com>
2022-11-15 19:43:06 -0700

    trying to add icons still

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,12 +6,17 @@ function Weather() {
   //get zipcode
   const [zipcode, setZipcode] = React.useState("");
   const [forecast, setForecast] = React.useState();
-
+  const [image, setImage] = React.useState();
   const endpoint =
     "https://api.openweathermap.org/data/2.5/weather?appid=12ea3c9f921a4ca10f046151c2b64c99&units=imperial&zip=";
 
+  //get image for weather
+  // http://openweathermap.org/img/wn/
+  const icon = "http://openweathermap.org/img/wn/";
+
   //get infor from the API
-  const fetchWeather = () => {
+  const fetchWeather = (event) => {
+    event.preventDefault();
     //set up url to include zipcode
     let url = `${endpoint}${zipcode}`;
     console.log(url);
@@ -21,8 +26,13 @@ function Weather() {
     });
     setZipcode("");
   };
-  // React.useEffect(() => {}, []);
 
+  const fetchIcon = () => {
+    let imageIcon = `${icon}${forecast?.weather[0].icon}`;
+    setImage(imageIcon);
+  };
+
+  // }
   // get info from input box
   const handleTextBox = (event) => {
     // console.log("input");
@@ -31,22 +41,23 @@ function Weather() {
   };
   return (
     <div className="container">
-      <input
-        type="number"
-        placeholder="Enter Zipcode"
-        onChange={handleTextBox}
-        value={zipcode}
-      ></input>
-      <button type="submit" onClick={fetchWeather}>
-        Submit
-      </button>
+      <form onSubmit={fetchWeather} className="form">
+        <input
+          type="number"
+          placeholder="Enter Zipcode"
+          onChange={handleTextBox}
+          value={zipcode}
+        ></input>
+        <button>Submit</button>
+      </form>
       <div>
         <h1>Showing Weather for {forecast?.name}</h1>
-        <p>Temperature: {forecast?.main.temp}&deg; F</p>
+        <p className="temperature">Temperature: {forecast?.main.temp}&deg; F</p>
         <p className="conditions">
           Conditions: {forecast?.weather[0].description}
         </p>
         <p>Wind: {forecast?.wind.speed} mph</p>
+        <div>{image}</div>
       </div>
     </div>
   );
